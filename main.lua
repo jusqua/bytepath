284cfd081db57401b5fe51b33b71223bda6674a1
Ádrian Gama <adrian.gama.se@gmail.com>
2024-06-19 20:58:38 -0300

    create base scene with shakable circle

--- main.lua
+++ main.lua
@@ -1,19 +1,56 @@
+local Camera = require('lib.hump.camera')
+local push = require('lib.push.push')
+local Input = require('lib.input.input')
 local Engine = require('src.Engine')
+local M = require('src.utils')
 
-local engine
+local VIRTUAL_WIDTH, WIDTH, VIRTUAL_HEIGHT, HEIGHT, WINDOW_SCALE
+local engine, camera
 
 function love.load()
   engine = Engine()
+
+  VIRTUAL_WIDTH, VIRTUAL_HEIGHT, _ = love.window.getMode()
+  WINDOW_SCALE = 3
+  WIDTH, HEIGHT = VIRTUAL_WIDTH * WINDOW_SCALE, VIRTUAL_HEIGHT * WINDOW_SCALE
+
+  love.window.setMode(WIDTH, HEIGHT)
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+  love.graphics.setLineStyle('rough')
+  push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, { upscale = 'pixel-perfect', canvas = true })
+
+  camera = Camera()
+
+  Input.bind_callbacks()
 end
 
 function love.update(dt)
   engine:update(dt)
 
-  if love.keyboard.isDown('escape') then
+  camera:lockPosition(WIDTH / 2, HEIGHT / 2, Camera.smooth.damped(5))
+
+  local escape_pressed, _, _ = Input.pressed('escape')
+  if escape_pressed then
     love.event.quit()
+  end
+
+  local s_pressed, _, _ = Input.pressed('s')
+  if s_pressed then
+    M.shake(camera, engine.timer, 4, 60, 1)
   end
 end
 
 function love.draw()
+  push:start()
+  camera:attach()
+
   engine:draw()
+  love.graphics.circle('line', VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT / 2, 64)
+
+  camera:detach()
+  push:finish()
+end
+
+function love.resize(width, height)
+  push.resize(width, height)
 end
