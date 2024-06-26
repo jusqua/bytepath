782621fe8c1b86ffbb2ba744b348f091f6dd8e82
Ádrian Gama <adrian.gama.se@gmail.com>
2024-06-26 20:05:01 -0300

    add assassin ship

---
+++ src/ships/Assassin.lua
@@ -0,0 +1,75 @@
+local Object = require('lib.classic.classic')
+local TrailParticle = require('src.TrailParticle')
+
+local Assassin = Object:extend()
+
+function Assassin:new(player)
+  self.player = player
+  self.width = 12
+  self.height = 12
+  self.polygons = {
+    {
+      self.width,
+      0, -- 1
+      -self.width / 3,
+      -self.width / 2, -- 2
+      -self.width,
+      0, -- 3
+      -self.width / 3,
+      self.width / 2, -- 4
+    },
+    {
+      self.width,
+      0, -- 5
+      self.width * 2 / 3,
+      -self.width / 3, -- 6
+      -self.width * 3 / 4,
+      -self.width * 4 / 3, -- 7
+      -self.width / 3,
+      -self.width / 2, -- 8
+    },
+    {
+      self.width,
+      0, -- 9
+      self.width * 2 / 3,
+      self.width / 3, -- 10
+      -self.width * 3 / 4,
+      self.width * 4 / 3, -- 11
+      -self.width / 3,
+      self.width / 2, -- 12
+    },
+  }
+end
+
+function Assassin:trails()
+  local trails = {}
+
+  local d = 0.9 * self.width
+  local x = self.player.x - d * math.cos(self.player.angle)
+  local y = self.player.y - d * math.sin(self.player.angle)
+  local offset = 1.3 * self.width
+
+  table.insert(trails, TrailParticle(x, y, { radius = love.math.random(1.0, 2.2), color = self.player.trail_color }))
+
+  table.insert(
+    trails,
+    TrailParticle(
+      x + offset * math.cos(self.player.angle - math.pi / 2),
+      y + offset * math.sin(self.player.angle - math.pi / 2),
+      { radius = love.math.random(0.5, 1.5), color = self.player.trail_color }
+    )
+  )
+
+  table.insert(
+    trails,
+    TrailParticle(
+      x + offset * math.cos(self.player.angle + math.pi / 2),
+      y + offset * math.sin(self.player.angle + math.pi / 2),
+      { radius = love.math.random(0.5, 1.5), color = self.player.trail_color }
+    )
+  )
+
+  return trails
+end
+
+return Assassin
--- src/ships/init.lua
+++ src/ships/init.lua
@@ -1,7 +1,9 @@
 local Fighter = require('src.ships.Fighter')
 local Prism = require('src.ships.Prism')
+local Assassin = require('src.ships.Assassin')
 
 return {
   Fighter,
   Prism,
+  Assassin,
 }
