581f1a095394818c54841e6ebdf7ddc37af54091
Ádrian Gama <adrian.gama.se@gmail.com>
2024-06-26 20:32:16 -0300

    add hunter ship

---
+++ src/ships/Hunter.lua
@@ -0,0 +1,62 @@
+local Object = require('lib.classic.classic')
+local TrailParticle = require('src.TrailParticle')
+
+local Hunter = Object:extend()
+
+function Hunter:new(player)
+  self.player = player
+  self.width = 12
+  self.height = 12
+  self.polygons = {
+    {
+      self.width * 0.7,
+      0,
+      self.width,
+      -self.width * 0.3,
+      self.width * 0.7,
+      -self.width * 0.7,
+      -self.width * 0.7,
+      -self.width * 0.7,
+      -self.width,
+      -self.width * 0.5,
+      -self.width,
+      self.width * 0.5,
+      -self.width * 0.7,
+      self.width * 0.7,
+      self.width * 0.7,
+      self.width * 0.7,
+      self.width,
+      self.width * 0.3,
+    },
+    {
+      0,
+      -self.width,
+      self.width * 0.5,
+      -self.width * 0.7,
+      -self.width * 0.5,
+      -self.width * 0.7,
+    },
+    {
+      0,
+      self.width,
+      self.width * 0.5,
+      self.width * 0.7,
+      -self.width * 0.5,
+      self.width * 0.7,
+    },
+  }
+end
+
+function Hunter:trails()
+  local trails = {}
+
+  local d = 1.2 * self.width
+  local x = self.player.x - d * math.cos(self.player.angle)
+  local y = self.player.y - d * math.sin(self.player.angle)
+
+  table.insert(trails, TrailParticle(x, y, { radius = love.math.random(3, 6), color = self.player.trail_color }))
+
+  return trails
+end
+
+return Hunter
--- src/ships/init.lua
+++ src/ships/init.lua
@@ -2,10 +2,12 @@ local Fighter = require('src.ships.Fighter')
 local Prism = require('src.ships.Prism')
 local Assassin = require('src.ships.Assassin')
 local Sloth = require('src.ships.Sloth')
+local Hunter = require('src.ships.Hunter')
 
 return {
   Fighter,
   Prism,
   Assassin,
   Sloth,
+  Hunter,
 }
