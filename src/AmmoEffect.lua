d723a6709c97f10babcf0caa99e14be84fff4ac5
Ádrian Gama <adrian.gama.se@gmail.com>
2024-06-27 00:51:21 -0300

    simplify ammo effect

--- src/AmmoEffect.lua
+++ src/AmmoEffect.lua
@@ -1,5 +1,5 @@
 local GameObject = require('src.GameObject')
-local constants  = require('src.constants')
+local constants = require('src.constants')
 
 local AmmoEffect = GameObject:extend()
 
@@ -7,19 +7,21 @@ function AmmoEffect:new(x, y)
   AmmoEffect.super.new(self, x, y)
 
   self.width, self.height = 8, 8
-  self.alpha = 1
-  self.timer:tween(0.5, self, { alpha = 0, width = 12, height = 12 }, 'in-out-quad', function ()
-    self:die()
+  self.color = constants.default_color
+  self.timer:after(0.1, function()
+    self.color = constants.ammo_color
+    self.timer:after(0.15, function()
+      self:die()
+    end)
   end)
 end
 
 function AmmoEffect:draw()
-  local r, g, b = unpack(constants.ammo_color)
   love.graphics.push()
   love.graphics.translate(self.x, self.y)
   love.graphics.rotate(math.pi / 4)
   love.graphics.translate(-self.x, -self.y)
-  love.graphics.setColor(r, g, b, self.alpha)
+  love.graphics.setColor(self.color)
   love.graphics.rectangle('fill', self.x - self.width / 2, self.y - self.height / 2, self.width, self.height)
   love.graphics.setColor(constants.default_color)
   love.graphics.pop()
