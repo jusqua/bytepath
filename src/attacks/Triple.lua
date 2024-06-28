f8f9764f177246160bf9b1521b644d241c693389
Ádrian Gama <adrian.gama.se@gmail.com>
2024-06-28 00:15:40 -0300

    add triple attack

---
+++ src/attacks/Triple.lua
@@ -0,0 +1,48 @@
+local Object = require('lib.classic.classic')
+local Projectile = require('src.Projectile')
+local colors = require('src.constants.colors')
+
+local Triple = Object:extend()
+
+function Triple:new(player)
+  self.cooldown = 0.32
+  self.cost = 3
+  self.abbreviation = '3'
+  self.player = player
+end
+
+function Triple:projectiles()
+  local projectiles = {}
+  local player = self.player
+  local d = player.width * 1.8
+  local offset = math.pi / 12
+  local color = colors.normal.boost
+  local angle = player.angle
+  local area = player.area
+  local x, y = player.x, player.y
+
+  table.insert(
+    projectiles,
+    Projectile(x + d * math.cos(angle), y + d * math.sin(angle), { angle = angle, area = area, color = color })
+  )
+  table.insert(
+    projectiles,
+    Projectile(
+      x + d * math.cos(angle + offset),
+      y + d * math.sin(angle + offset),
+      { angle = angle + offset, area = area, color = color }
+    )
+  )
+  table.insert(
+    projectiles,
+    Projectile(
+      x + d * math.cos(angle - offset),
+      y + d * math.sin(angle - offset),
+      { angle = angle - offset, area = area, color = color }
+    )
+  )
+
+  return projectiles
+end
+
+return Triple
--- src/attacks/init.lua
+++ src/attacks/init.lua
@@ -1,4 +1,5 @@
 return {
   Neutral = require('src.attacks.Neutral'),
   Double = require('src.attacks.Double'),
+  Triple = require('src.attacks.Triple'),
 }
