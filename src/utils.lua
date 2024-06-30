e569d0c9ea0d633c8eb2112d9bc8d502a4b765e2
Ádrian Gama <adrian.gama.se@gmail.com>
2024-06-29 21:52:47 -0300

    add chance list generator

--- src/utils.lua
+++ src/utils.lua
@@ -100,6 +100,23 @@ local function generateIrregularPolygon(size, amount)
   return points
 end
 
+local function chanceList(...)
+  return {
+    chance_list = {},
+    chanch_definitions = { ... },
+    next = function(self)
+      if #self.chance_list == 0 then
+        for _, chance_definition in ipairs(self.chance_definitions) do
+          for _ = 1, chance_definition[2] do
+            table.insert(self.chance_list, chance_definition[1])
+          end
+        end
+      end
+      return table.remove(self.chance_list, love.math.random(1, #self.chance_list))
+    end,
+  }
+end
+
 return {
   shakeCamera = shakeCamera,
   getWindowDimensions = getWindowDimensions,
@@ -112,4 +129,5 @@ return {
   rotateAtPosition = rotateAtPosition,
   rotateAtPositionAndScale = rotateAtPositionAndScale,
   generateIrregularPolygon = generateIrregularPolygon,
+  chanceList = chanceList,
 }
